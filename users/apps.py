d5d86a8197f1cee8a6e5a0b645c2da917a464664
GayratovJavohir <javohirgayratov03@gmail.com>
2026-03-28 16:47:02 +0500

    restart everthing

--- users/apps.py
+++ users/apps.py
@@ -4,15 +4,3 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@gmail.com",
-        password="12345678"
-    )
